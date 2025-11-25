e872d37c358702f84ea875ba304be816f5e39e9c
Kevin Spalding <kevinspalding@usf.edu>
2025-11-24 20:40:09 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-print("Welcome to my Python program!")
-hours = input("How many hours did you study today? ")
+print("Welcome to my Python program!") #Welcome message 
+hours = input("How many hours did you study today? ") #Hours studied input
 hours = float(hours)
 weekly_hours = hours * 7
 print(f"You are on track to study {weekly_hours} hours this week.")
 try:
     hours = float(hours)
 except ValueError:
-    print("Please enter a valid number.")
+    print("Please enter a valid number.") #Basic error handling
     exit()
